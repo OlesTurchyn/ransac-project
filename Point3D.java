eaf52bdc5fd7ea8916938078cc0e468156c7f4bb
Oleksander Turchyn <oleksander.turchyn@gmail.com>
2023-01-31 17:42:53 -0500

    Initialized classes for Part 1

---
+++ Point3D.java
@@ -0,0 +1,28 @@
+public class Point3D {
+    /*getX, getY and getZ methods returning double
+        ▪ public double getX()
+        ▪ public double getY()
+        ▪ public double getZ()
+     */
+
+     public double getX(){
+        double x = 0.0;
+
+        return x;
+     }
+
+     public double getY(){
+        double y = 0.0;
+
+        return y;
+     }
+
+     public double getZ(){
+        double z = 0.0;
+
+        return z;
+     }
+
+     
+    
+}
