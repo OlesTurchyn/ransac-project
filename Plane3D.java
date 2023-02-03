ce525859ab78944d7cacd043216bf8dfb779bb0a
Oleksander Turchyn <oleksander.turchyn@gmail.com>
2023-02-03 14:49:58 -0500

    Pseudocode

--- Plane3D.java
+++ Plane3D.java
@@ -5,6 +5,20 @@ public class Plane3D {
     
     public Plane3D(Point3D p1, Point3D p2, Point3D p3){
 
+        //p1 = Plane3D(p1.x, p1.y, p1.z, 1.00);
 
+        //xyz xyz xyz 
+        
+        //Compute the equation of the plane with the 3 points using the
+        //Plane3D(a,b,c,d) method as a constructor
+
+        //The plane will look like: 
+        //(Plane3D.a) x + (Plane3D.b) y + (Plane3D.c) z = (Plane3D.d)
+
+
+    }
+
+    public Plane3D(double a, double b, double c, double d){
+        
     }
 }
