9f97f57521b3e60af1efa8ea59f5d1efad526442
Oleksander Turchyn <52382133+OlesTurchyn@users.noreply.github.com>
2023-02-04 03:04:43 -0500

    Added Key Methods for PointCloud

--- PointCloud.java
+++ PointCloud.java
@@ -2,5 +2,32 @@
 //STUDENT NUMBER: 300174825
 
 public class PointCloud {
+
+    //Constructor
+    PointCloud(String filename){
+
+    }
+
+    //Empty Constructor makes an empty PointCloud
+    PointCloud(){
+
+    }
+
+    //Adds point to the PointCloud
+    public void addPoint(Point3D pt){
+
+    }
+
+    //Returns a random point from the PointCloud
+    Point3D getPoint(){
+        return new Point3D(0, 0, 0);
+    }
+
+    //Saves the PointCloud into a .xyz file
+    public void save(String filename){
+
+    }
+
+
     
 }
